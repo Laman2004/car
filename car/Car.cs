7b0e6a8afc94a236eacc64ae7ec7e7863e13c55a
Leman <Leman@COMPUTER-SCIENC>
2023-03-30 22:58:11 +0400

    Add project files.

---
+++ car/Car.cs
@@ -0,0 +1,63 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace car
+{
+    internal class Car
+    {
+        private string _brand;
+        private string _model;
+        private double _fuelcapacity;
+        private double _nowcapacity;
+        private double _fuelfor1km;
+        public string Brand { get
+            {
+                return this._brand;
+            }
+            set {
+                if (value.Length >= 2 && value.Length <= 200)
+                    this._brand = value;
+            } }
+        public string Model { 
+
+            get=>this._model;
+            
+            set
+            {
+                if (value.Length >= 1 && value.Length <= 10)
+                    this._model = value;
+            } }
+        public double FuelCapacity { get
+            {
+                return this._fuelcapacity;
+            }
+            set
+            {
+                if (value >= 30 && value <= 200)
+                    this._fuelcapacity = value;
+            } }
+        public double NowCapacity { get
+            {
+                return this._nowcapacity;
+            }
+            set
+            {
+                if (value >= 0 && value <= 200)
+                    this._nowcapacity = value;
+            } }
+        public double FuelFor1km { get;
+            private set;  }
+        public string Drive(double km) {
+            this.FuelFor1km = this._nowcapacity / km;
+            
+            return ($"Brand : {Brand} - Model : {Model} - FuelCapacity : {FuelCapacity} - NowCapacity : {NowCapacity} - FuelFor1km : {FuelFor1km}");
+        }
+        public string GetInfo()
+        {
+            return ($"Brand : {Brand} - Model : {Model} - FuelCapacity : {FuelCapacity} - NowCapacity : {NowCapacity} - FuelFor1km : {FuelFor1km}");
+        }
+    }
+}
---
+++ car/Program.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace car
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Fuelcapacity-ni daxil edin");
+            double benzin;
+            string str=Console.ReadLine();
+            while(!double.TryParse(str, out benzin))
+            {
+                Console.WriteLine("Daxil etdiyiniz deyer duzgun deyil");
+                str = Console.ReadLine();
+            }
+            Console.WriteLine("NowCapacity-ni daxil edin:");
+            double fuel;
+            string str2 = Console.ReadLine();
+            while(!double.TryParse(str2, out fuel))
+            {
+                Console.WriteLine("Daxil etdiyiniz deyer duzgun deyil");
+                str2 = Console.ReadLine();
+            }
+            Car car = new Car
+            {
+                Brand = Console.ReadLine(),
+                Model = Console.ReadLine(),
+                FuelCapacity =double.Parse(str),
+                NowCapacity =double.Parse(str2)
+            };
+            
+            Console.WriteLine(car.Drive(20));
+            Console.WriteLine( car.GetInfo());
+        }
+    }
+}
